e9190c79e3d566fb1d70e82f2e94e72ee2848213
Long30112002 <tuanlong30112@gmail.com>
2026-03-26 21:19:29 +0700

    Initial commit: Clean and Professional Structure

---
+++ ealthcare-connect-backend/src/main/java/com/hoanglong/ealthcare_connect/EalthcareConnectApplication.java
@@ -0,0 +1,13 @@
+package com.hoanglong.ealthcare_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EalthcareConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EalthcareConnectApplication.class, args);
+	}
+
+}
