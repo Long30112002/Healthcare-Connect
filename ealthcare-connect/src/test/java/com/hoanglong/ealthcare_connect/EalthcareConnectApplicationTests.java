2a529ecc37bb33eebe30c277275e44cd20e4f163
Long30112002 <tuanlong30112@gmail.com>
2026-03-26 20:11:50 +0700

    Initial: Setup Spring Boot project for Healthcare Connect with Clean Architecture

---
+++ ealthcare-connect/src/test/java/com/hoanglong/ealthcare_connect/EalthcareConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanglong.ealthcare_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EalthcareConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
